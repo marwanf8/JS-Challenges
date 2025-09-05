92546ac861894d9a2f87923ce913650d3744c8f1
marwanf8 <marwanfathy86@gmail.com>
2025-09-05 20:48:47 +0300

    First Challenge

---
+++ challenge-1 (is100).js
@@ -0,0 +1,13 @@
+/* 
+Todo: Exercise (01)
+write a JavaScript program to check two numbers and return
+true if one of the number is 100 or if the sum of the two numbers 
+is 100
+*/
+
+// get number = x, y
+// if x === 100 or y === 100 => true
+// if x + y === 100 -> true
+// else false
+
+const is100 = (x, y) => x ===100 || y === 100 || x + y === 100 ? true : false 
